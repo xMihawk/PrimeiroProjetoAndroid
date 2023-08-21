6f0d804af8ff6064a7595c2fbdf45a67813e0b6d
Aluno <matheus.holiveira10@gmail.com>
2023-08-21 20:43:02 -0300

    Primeiro Projeto

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,18 +1,33 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
 import android.widget.Toast
+
+
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var button1: Button = findViewById(R.id.button)
-        button1.setOnClickListener {
-            Toast.makeText( this, "Clicou no botao", Toast.LENGTH_LONG).show()
-        }
+
+//        var button1: Button = findViewById(R.id.button)
+//         button1.setOnClickListener {
+//             Toast.makeText( this, "Usuario Logado!", Toast.LENGTH_LONG).show()
+//         }
+
+    }
+
+    fun proxTela(view: View){
+        val intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent);
+    }
+
+    fun fechaTela(view: View){
+        Toast.makeText(this, "", Toast.LENGTH_LONG).show()
+        finish();
     }
 }
